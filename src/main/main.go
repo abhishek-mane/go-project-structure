89524928e441646dcad88e6eba4bf2eb8f2f2380
Abhishek Mane <abhishekmane@outlook.in>
2017-12-13 23:51:37 +0530

    init

---
+++ src/main/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"lib/logger"
+
+	_ "lib/models"
+)
+
+func main() {
+	fmt.Println("Hello World !")
+
+	log := logger.GetLogger()
+
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
+
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 122,
+	}).Warn("The group's number increased tremendously!")
+
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 100,
+	}).Fatal("The ice breaks!")
+
+	// A common pattern is to re-use fields between logging statements by re-using
+	// the logrus.Entry returned from WithFields()
+	contextLogger := log.WithFields(log.Fields{
+		"common": "this is a common field",
+		"other":  "I also should be logged always",
+	})
+
+	contextLogger.Info("I'll be logged with common and other field")
+	contextLogger.Info("Me too")
+}
